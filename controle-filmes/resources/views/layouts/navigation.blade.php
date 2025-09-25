8434bbe1d915d7e81ffb9a1bc3199941ece1ad9d
Leony Leandro Barros <leonyleandrobarros@gmail.com>
2025-09-25 10:56:23 -0300

    Foi
    É isso mesmo!!!

--- controle-filmes/resources/views/layouts/navigation.blade.php
+++ controle-filmes/resources/views/layouts/navigation.blade.php
@@ -68,7 +68,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('movies.index')" :active="request()->routeIs('movies.index')">
-                {{ __('Dashboard') }}
+                {{ __('movies.index') }}
             </x-responsive-nav-link>
         </div>
 
